8fff6037e8e003317ec26d244a301f91106eb71c
JustDoIt-hub <78211928+JustDoIt-hub@users.noreply.github.com>
2025-05-05 20:26:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def get_scoreboard():
     return sorted(teams, key=lambda x: (x["points"], x["goal_difference"], x["goals_for"]), reverse=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
